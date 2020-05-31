6755d5269f83b866a6b507e7b9849cb8ba7f6f82
Halfwhit <Halfwhit@gmail.com>
2020-05-31 17:30:47 +0100

    Added search functionality

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,18 @@
-use std::{env, fs, process, error::Error};
+use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {} in file {}.", config.query, config.filename);
-
-    run(config);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments provided, requires a query and a filename");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("Complete text:\n{}", contents);
-    Ok(())
 }
