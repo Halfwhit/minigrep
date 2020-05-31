1949b9aae52fb6846b92341fc300fae33af2dc0e
Halfwhit <Halfwhit@gmail.com>
2020-05-31 12:48:54 +0100

    Read command line-arguments Read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // $arg[0] contains the program's relative path and name
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for '{}' in the file named '{}'.\n", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("Complete text:\n{}\n", contents)
 }
